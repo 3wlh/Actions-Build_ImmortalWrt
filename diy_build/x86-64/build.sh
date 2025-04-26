d7853e0100efb6a691f8cc8cac5d3aa2239d0d27
3wlh <1094890624@qq.com>
2025-04-26 17:25:12 +0800

    Update build.sh

--- diy_build/x86-64/build.sh
+++ diy_build/x86-64/build.sh
@@ -1,61 +1,130 @@
 #!/bin/bash
-# Log file for debugging
-LOGFILE="/tmp/uci-defaults-log.txt"
-echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
-echo "编译固件大小为: $PROFILE MB"
-echo "Include Docker: $INCLUDE_DOCKER"
+#################### 函数 ####################
+function Replace(){ # 修改函数
+[[ -f "$(pwd)/.config" ]] || return
+if [[ -n "${2}" ]]; then
+	sed -i "s/.*${1}.*/${1}=${2}/g" "$(pwd)/.config"
+else
+	sed -i "s/.*${1}.*/# ${1} is not set/g" "$(pwd)/.config"
+fi
+}
 
-echo "Create pppoe-settings"
+function Download(){ # 下载函数
+echo "Downloading ${1}"
+curl -# --fail "${1}" -o "$(pwd)/packages/diy_packages/$(basename ${1})"
+# #wget -qO "$(pwd)/packages/diy_packages/$(basename $Download_URL)" "${Download_URL}" --show-progress
+}
+
+function Segmentation(){ # 分割下载函数
+[[ -d "$(pwd)/packages/diy_packages" ]] || mkdir -p "$(pwd)/packages/diy_packages"
+echo "Download_Path: $(pwd)/packages/diy_packages"
+PACKAGES_URL="${1}"
+PACKAGES_NAME=(${2})
+wget -qO- "${PACKAGES_URL}" | \
+while IFS= read -r LINE; do
+    for PREFIX in "${PACKAGES_NAME[@]}"; do
+        if [[ "$LINE" == *"$PREFIX"* ]]; then
+            FILE=$(echo "$LINE" | grep -Eo 'href="[^"]*' | sed 's/href="//')
+            if [[ -z "$FILE" ]]; then
+                # echo "No file found in line, skipping"
+                continue
+            fi
+            Download_URL="${PACKAGES_URL}${FILE}"
+            Download "${Download_URL}"
+            curl -# --fail "$Download_URL" -o "$(pwd)/packages/diy_packages/$(basename $Download_URL)"
+        fi
+    done
+done
+}
+###################################################################
+
+echo "==============================下载插件=============================="
+# sed -i '1a src/gz openwrt_kiddin9 https://dl.openwrt.ai/releases/24.10/packages/aarch64_generic/kiddin9' "repositories.conf"
+# sed -i "s/option check_signature/# option check_signature/g" "repositories.conf"
+[[ -d "$(pwd)/packages/diy_packages" ]] || mkdir -p "$(pwd)/packages/diy_packages"
+echo "Download_Path: $(pwd)/packages/diy_packages"
+#Segmentation "https://dl.openwrt.ai/releases/24.10/packages/x86_64/kiddin9/" \
+#"luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel luci-app-sunpanel"
+Segmentation "https://op.dllkids.xyz/packages/x86_64/" \
+"luci-app-unishare unishare webdav2 luci-app-v2ray-server sunpanel luci-app-sunpanel"
+echo "=========================== 查看下载插件 ==========================="
+ls $(pwd)/packages/diy_packages
+echo "============================= 镜像信息 ============================="
+echo "路由器型号: $PROFILE"
+echo "固件大小: $ROOTFS_PARTSIZE"
+#========== 创建自定义配置文件 ==========# 
 mkdir -p  /home/build/immortalwrt/files/etc/config
-
-# 创建pppoe配置文件 yml传入环境变量ENABLE_PPPOE等 写入配置文件 供99-custom.sh读取
-cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
+cat << EOF > /home/build/immortalwrt/files/etc/config/diy-settings
 enable_pppoe=${ENABLE_PPPOE}
 pppoe_account=${PPPOE_ACCOUNT}
 pppoe_password=${PPPOE_PASSWORD}
 EOF
-
-echo "cat pppoe-settings"
-cat /home/build/immortalwrt/files/etc/config/pppoe-settings
-# 输出调试信息
-echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
-
-
-
-# 定义所需安装的包列表 下列插件你都可以自行删减
+echo "========================= 查看自定义配置 ========================="
+cat /home/build/immortalwrt/files/etc/config/diy-settings
+echo "================================================================="
+#=============== 开始构建镜像 ===============#
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建镜像..."
+#========== 定义所需安装的包列表 ==========#
 PACKAGES=""
-PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES bash busybox uci luci uhttpd luci-base opkg curl openssl-util"
+PACKAGES="$PACKAGES coremark ds-lite e2fsprogs htop kmod-lib-zstd"
+PACKAGES="$PACKAGES lsblk nano resolveip swconfig wget-ssl zram-swap"
+# USB驱动
+PACKAGES="$PACKAGES kmod-usb-core kmod-usb2 kmod-usb3 kmod-usb-ohci kmod-usb-storage kmod-scsi-generic"
+PACKAGES="$PACKAGES kmod-nft-offload kmod-nft-fullcone kmod-nft-nat"
+# 23.05.4 luci-i18n-opkg-zh-cn
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-base-zh-cn" 
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
-PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
+# PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ramfree-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-app-unishare"
+PACKAGES="$PACKAGES luci-app-v2ray-server"
+PACKAGES="$PACKAGES sunpanel luci-app-sunpanel"
+# PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
+# DDNS解析
+PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn ddns-scripts_aliyun ddns-scripts-cloudflare ddns-scripts-dnspod"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-
-# 判断是否需要编译 Docker 插件
-if [ "$INCLUDE_DOCKER" = "yes" ]; then
-    PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
-    echo "Adding package: luci-i18n-dockerman-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# 添加Docker插件
+if $INCLUDE_DOCKER; then
+    PACKAGES="$PACKAGES docker-compose luci-i18n-dockerman-zh-cn"
+    echo "添加Package: luci-i18n-dockerman-zh-cn"
 fi
+#========== 删除插件包 ==========#
+PACKAGES="$PACKAGES -luci-app-cpufreq"
 
-# 构建镜像
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
+#=============== 开始打包镜像 ===============#
+echo "============================= 默认插件 ============================="
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 默认插件包："
+echo "$(make info | grep "Default Packages:" | sed 's/Default Packages: //')"
+echo "=========================== 编译添加插件 ==========================="
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 编译添加插件："
 echo "$PACKAGES"
+echo "============================ 编辑Config ============================"
+Replace "CONFIG_TARGET_EXT4_JOURNAL"
+Replace "CONFIG_TARGET_KERNEL_PARTSIZE" "32"
+Replace "CONFIG_TARGET_ROOTFS_PARTSIZE" "${ROOTFS_PARTSIZE}"
+Replace "CONFIG_ISO_IMAGES"
+Replace "CONFIG_QCOW2_IMAGES"
+Replace "CONFIG_VDI_IMAGES"
+Replace "CONFIG_VMDK_IMAGES"
+Replace "CONFIG_VHDX_IMAGES"
+echo "============================= 打包镜像 ============================="
+cp -f "$(pwd)/.config" "$(pwd)/bin/buildinfo.config"
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
 
-make image PROFILE="generic" PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$PROFILE
-
+echo "============================= 构建结果 ============================="
 if [ $? -ne 0 ]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - 打包镜像失败!"
     exit 1
 fi
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 打包镜像完成."
