139eb238b524c84425f14f380422f5167f431b38
3wlh <1094890624@qq.com>
2025-04-27 15:32:00 +0800

    Update build.sh

--- diy_build/x86-64/build.sh
+++ diy_build/x86-64/build.sh
@@ -48,15 +48,21 @@ while IFS= read -r LINE; do
     [[ -z "${name}" || -z "${url}" ]] && continue
     echo "Downloading ${url}/Packages.gz"
     curl -# --fail "${url}/Packages.gz" -o "/tmp/Packages.gz"
-    [[ -f "/tmp/Packages.gz" || -f "$(pwd)/dl/${name}" ]] || continue
+    [[ -f "/tmp/Packages.gz" && -f "$(pwd)/dl/${name}" ]] || continue
     md5url=$(md5sum -b "/tmp/Packages.gz" | awk '{print $1}')
     md5name=$(md5sum -b "$(pwd)/dl/${name}" | awk '{print $1}')
     echo "md5sum: ${md5url}  ${md5name}"
     [[ -z "${md5url}" || -z "${md5name}" ]] && continue
-    [[ "${md5url}" == "${md5name}" ]] || rm -rf "$(pwd)/dl" && echo -e "删除所有缓存插件！" && break
-    echo "${name} 无更新插件."
+    if [[ "${md5url}" == "${md5name}" ]]; then
+        echo "${name} 无更新插件."
+    else
+        rm -rf "$(pwd)/dl"
+        echo -e "删除所有缓存插件！" 
+        break
+    fi
 done
 }
+
 ###################################################################
 find . -maxdepth 1 -type f -name "repositories.conf" -exec cp {} "$(pwd)/packages/" \;
 
