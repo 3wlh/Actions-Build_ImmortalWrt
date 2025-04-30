1829e620eadb269f070eb8251e4a905a5fe236a1
3wlh <1094890624@qq.com>
2025-05-01 00:52:05 +0800

    up

--- diy_build/armv8/make.sh
+++ diy_build/armv8/make.sh
@@ -102,7 +102,7 @@ Replace "CONFIG_TARGET_ROOTFS_PARTSIZE" "${ROOTFS_PARTSIZE}"
 cp -f "$(pwd)/.config" "$(pwd)/bin/buildinfo.config"
 echo "============================= 打包镜像 ============================="
 cp -f "$(pwd)/repositories.conf" "$(pwd)/bin/repositories.conf"
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
+# make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
 echo "============================= 构建结果 ============================="
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - 打包镜像失败!"
