0a9fbbf64b97a1566c8379aee5ed8b3bf81f2cd6
3wlh <1094890624@qq.com>
2025-06-09 19:26:10 +0800

    Update Kmods.sh

--- .github/.sh/Kmods.sh
+++ .github/.sh/Kmods.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+Kernel=$(find "$(pwd)/build_dir/" -type f -name "kernel_*.ipk" 2>/dev/null)
+[[ -z "${Kernel}" ]] && Kernel=$(find "$(pwd)/build_dir/" -type f -name "kernel_*.apk" 2>/dev/null)
+[[ -n "${Kernel}" ]] && echo "$(date '+%Y-%m-%d %H:%M:%S') - Kernel版本：$(basename ${Kernel})"
 cat "$(pwd)/repositories.conf" | \
 while IFS= read -r LINE; do
     [[ -z "$(echo "${LINE}" | grep -Eo "^src/gz .*kmods")" ]] && continue
