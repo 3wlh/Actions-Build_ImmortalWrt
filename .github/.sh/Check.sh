8d1736136b5797fc3cc9b0c852d379df2c43e718
3wlh <1094890624@qq.com>
2025-06-17 10:22:19 +0800

    UP

--- .github/.sh/Check.sh
+++ .github/.sh/Check.sh
@@ -16,7 +16,7 @@ while IFS= read -r LINE; do
         echo "${name} 无更新插件."
     else
         # 删除 GitHub 缓存
-        echo "cache=delete" >> "$(pwd)/bin/.bashrc"
+        echo "del_cache=success" >> "$(pwd)/bin/.bashrc"
         rm -rf "$(pwd)/dl"
         echo -e "删除所有缓存插件！" 
         break
